47aeb5690aab59a3b5145d970161375233e96a1b
Joddie <Jers.033@gmail.com>
2024-11-14 12:33:20 -0600

    add vite vue-js code

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HelloWorld
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  routes
+})
+
+export default router
